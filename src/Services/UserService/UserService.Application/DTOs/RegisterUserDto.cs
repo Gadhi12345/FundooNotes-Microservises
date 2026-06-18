06f1ebfc0f1c1edc68608ca14364318815c22e57
Adarsh <adarshniharika20@gmail.com>
2026-06-18 22:14:20 +0530

    Initial microservice solution setup

---
+++ src/Services/UserService/UserService.Application/DTOs/RegisterUserDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Application.DTOs
+{
+    internal class RegisterUserDto
+    {
+    }
+}
---
+++ src/Services/UserService/UserService.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿
+namespace UserService.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+
+    }
+}
