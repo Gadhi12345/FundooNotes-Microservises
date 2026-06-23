cf1b66bd2d5f909d67a89c5f57cf28c6efd8a9d1
Adarsh <adarshniharika20@gmail.com>
2026-06-23 22:01:49 +0530

    Completed implementation of Redis Cache for Archive Notes Command in Notes Service

--- src/Services/NotesService/NotesService.Application/Handlers/Command Handlers/ArchiveNoteHandler.cs
+++ src/Services/NotesService/NotesService.Application/Handlers/Command Handlers/ArchiveNoteHandler.cs
@@ -7,19 +7,23 @@ namespace NotesService.Application.Handlers.CommandHandlers
     public class ArchiveNoteHandler : IRequestHandler<ArchiveNoteCommand, bool>
     {
         private readonly INoteRepository _noteRepository;
+        private readonly ICacheService _cacheService;
 
-        public ArchiveNoteHandler(INoteRepository noteRepository)
+        public ArchiveNoteHandler(INoteRepository noteRepository, ICacheService cacheService)
         {
             _noteRepository = noteRepository;
+            _cacheService = cacheService;
         }
 
         public async Task<bool> Handle(
             ArchiveNoteCommand request,
             CancellationToken cancellationToken)
         {
-            return await _noteRepository.ArchiveNote(
-                request.NoteId,
-                request.UserId);
+            var result = await _noteRepository.ArchiveNote(request.NoteId, request.UserId);
+
+            await _cacheService.RemoveAsync($"notes:{request.UserId}");
+
+            return result;
         }
     }
 }
